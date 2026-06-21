9361d87c8da791b40ea0a6a5afe2f3bd684ce26a
sandeep gupta <guprasandeep24@gmail.com>
2026-06-21 16:17:55 +0530

    js file update

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     firebaseUid: { type: String, required: true, unique: true }, // <--- Yeh hona zaroori hai
     credits: { type: Number, default: 10 },
+    
     createdAt: { type: Date, default: Date.now }
 });
 
