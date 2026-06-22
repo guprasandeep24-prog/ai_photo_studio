1742f6d9b503657b82a0b4608c9eabdbd226fdc2
sandeep gupta <guprasandeep24@gmail.com>
2026-06-22 15:37:00 +0530

    Fix: Cleaned Order model

--- models/Order.js
+++ models/Order.js
@@ -1,17 +1,12 @@
 const mongoose = require('mongoose');
 
-// models/Order.js mein change karein
-
 const orderSchema = new mongoose.Schema({
     userId: { type: String, required: true },
     category: { type: String, required: true },
     gender: { type: String, required: true },
     aiImageUrl: { type: String, required: true },
     status: { type: String, default: 'completed' },
-
     razorpayOrderId: { type: String, default: 'N/A' } 
-
 }, { timestamps: true });
 
-
-module.exports = mongoose.model('Order', OrderSchema);
+module.exports = mongoose.model('Order', orderSchema);
